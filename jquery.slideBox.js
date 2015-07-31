05ef9b55a9489c87f95b28eeafdf31db32c137ba
stevedavid <stevedavid@users.noreply.github.com>
2015-07-31 17:06:09 +0200

    Create jquery.slideBox.js

---
+++ jquery.slideBox.js
@@ -0,0 +1,182 @@
+;(function($) {
+
+    $.fn.extend({
+        slideBox: function(options) {
+            if (options && typeof(options) == 'object') {
+                options = $.extend({}, $.slideBox.defaults, options);
+            }
+
+            if($(this).length == 1) {
+                new $.slideBox(this, options);
+            } else if($(this).length > 1) {
+                console.error($.messages.severalElements);
+            } else if($(this).length == 0) {
+                console.error($.messages.zeroElement);
+            }
+
+            return this;
+        }
+    });
+
+    $.messages = {
+        severalElements: '[jQuery.slideBox] Error: you have targetted more than one slideBox. Please use a different selector.',
+        zeroElement: '[jQuery.slideBox] Error: could not find the slideBox in the DOM. Are you sure the selector is correct?'
+    };
+
+    $.position = {
+        horizontal: null,
+        vertical: null
+    };
+
+    $.slideBox = function(elem, option) {
+        var options  = option || $.slideBox.defaults
+            , threShold = typeof(options.target) != 'number' ? $(options.target).offset().top : options.target;
+
+        if($(elem).length === 1) {
+            $.slideBox.applyCss(elem, options);
+
+            $(window).on('scroll', function() {
+
+                if($(this).scrollTop() + $(window).height() > threShold) {
+                    if(!$(elem).is(':visible') && !$(elem).is(':animated')) {
+                        $.slideBox.showBox(elem, options);
+                    }
+                } else {
+                    if($(elem).is(':visible') && !$(elem).is(':animated')) {
+                        $.slideBox.hideBox(elem, options);
+                    }
+                }
+
+            });
+        }
+
+        return;
+    };
+
+    $.slideBox.applyCss = function(elem, options) {
+        var  position = options.position.split(' ')
+            , cssPlacement = {}
+            , propertiesNb
+            , keys
+            , value;
+
+        $.position.vertical = position[0];
+        $.position.horizontal = position[1];
+
+        switch(position[0]) {
+            case 'top':
+                cssPlacement.top = 0;
+                break;
+            case 'middle':
+                cssPlacement.top = '50%';
+                cssPlacement.marginTop = parseInt($(elem).height()) / 2 * -1;
+                break;
+            case 'bottom':
+                cssPlacement.bottom = 0;
+                break;
+        }
+
+        switch(position[1]) {
+            case 'left':
+                cssPlacement.left = 0;
+                break;
+            case 'center':
+                cssPlacement.left = '50%';
+                cssPlacement.marginLeft = parseInt($(elem).width()) / 2 * -1;
+                break;
+            case 'right':
+                cssPlacement.right = 0;
+                break;
+        }
+
+        keys = Object.keys(cssPlacement);
+        propertiesNb = keys.length;
+
+        for(var i = 0; i < propertiesNb; i++) {
+            $(elem).css(keys[i], cssPlacement[keys[i]]);
+        }
+
+        value = $.slideBox.calculateNewPosition(elem, options);
+        $(elem).css(options.appearsFrom, value + 'px');
+
+        $(elem).css({
+            display: 'none',
+            position: 'fixed'
+        });
+
+    };
+
+    $.slideBox.hideBox = function(elem, options) {
+
+        var animation = {};
+        animation[options.appearsFrom] = $.slideBox.calculateNewPosition(elem, options);
+
+        $(elem).animate(animation, {
+            duration: options.slideDuration,
+            complete: function() {
+                $(elem).trigger('sb.hidden');
+                $(elem).css('display', 'none');
+            }
+        })
+
+    };
+
+    $.slideBox.showBox = function(elem, options) {
+        $(elem).css('display', 'block');
+        var animation = {}
+            , value;
+
+        switch(options.appearsFrom) {
+            case 'top':
+            case 'bottom':
+                if($.position.vertical == options.appearsFrom) {
+                    value = 0;
+                } else {
+                    value = $(window).innerHeight() - $(elem).outerHeight();
+                }
+                break;
+            case 'left':
+            case 'right':
+                if($.position.horizontal == options.appearsFrom) {
+                    value = 0;
+                } else {
+                    value = $(window).innerWidth() - $(elem).outerWidth();
+                }
+                break;
+        }
+
+        animation[options.appearsFrom] = value;
+        $(elem).animate(animation, {
+            duration: options.slideDuration,
+            complete: function() {
+                $(elem).trigger('sb.shown');
+            }
+        });
+    };
+
+    $.slideBox.calculateNewPosition = function(elem, options) {
+        var width = $(elem).width()
+            , height = $(elem).height()
+            , value;
+
+        switch(options.appearsFrom) {
+            case 'right':
+            case 'left':
+                value = width * -1;
+                break;
+            case 'top':
+            case 'bottom':
+                value = height * -1;
+                break;
+        }
+
+        return value;
+    };
+
+    $.slideBox.defaults = {
+        position: 'bottom right',
+        appearsFrom: 'right',
+        slideDuration: 1500,
+        target: 1250
+    };
+})(jQuery);
